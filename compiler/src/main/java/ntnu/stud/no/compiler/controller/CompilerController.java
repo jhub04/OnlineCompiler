fba6023def294f7d3685f6ffef37ac66121e91bb
jonathan <jonathhu@stud.ntnu.no>
2025-02-13 15:49:06 +0100

    Add crossorigin

--- compiler/src/main/java/ntnu/stud/no/compiler/controller/CompilerController.java
+++ compiler/src/main/java/ntnu/stud/no/compiler/controller/CompilerController.java
@@ -1,5 +1,6 @@
 package ntnu.stud.no.compiler.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CompilerController {
 
+  @CrossOrigin(origins = "http://localhost:5173/")
   @PostMapping("/run")
   public String run(@RequestBody String code) {
     return "Ok!";
