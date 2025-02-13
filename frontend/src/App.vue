79a7bda5809a57d0240c9611938c447c4782d72c
jonathan <jonathhu@stud.ntnu.no>
2025-02-13 14:58:29 +0100

    Code input field

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,27 @@
+<script setup>
+
+import { ref } from "vue";
+
+const fileName = ref("main.cpp");
+const code = ref("public static void main(String[] args) {System.out.println(\"Hello World!\")}")
+const output = ref("");
+
+</script>
+
 <template>
-  <header>Online Compiler</header>
+  <header><h1>Java Compiler</h1></header>
   <main>
-    <h1>Hello!</h1>
+    <div class="input-container">
+      <p>{{ fileName }}</p>
+      <textarea>{{ code }}</textarea>
+    </div>
   </main>
 </template>
+
+<style scoped>
+textarea {
+  width: 750px;
+  height: 300px;
+  resize: none;
+}
+</style>
